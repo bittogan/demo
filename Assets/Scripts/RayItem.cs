34a15be37839a11a8202aa452766b783dbc0d520
bittogan <bittogan141@gmail.com>
2024-06-16 07:37:37 +0700

    VR

--- Assets/Scripts/RayItem.cs
+++ Assets/Scripts/RayItem.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class RayItem : MonoBehaviour , IRayItem
 {
+    [SerializeField] Color deActiveColor;
+    [SerializeField] Color activeColor;
+    [SerializeField] Renderer gameObjectRenderer;
     // Start is called before the first frame update
+    public void Start()
+    {
+        //gameObjectRenderer = GetComponent<Renderer>();
+        gameObjectRenderer.material.color = deActiveColor;
+    }
     public void OnPointerEnter()
     {
         Debug.Log("OnPointerEnter");
+        gameObjectRenderer.material.color = activeColor;
     }
         
     // Update is called once per frame
     public void OnPointerExit()
     {
         Debug.Log("OnPointerExit");
+        gameObjectRenderer.material.color = deActiveColor   ;
     }
 }
